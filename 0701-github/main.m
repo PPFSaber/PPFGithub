d954ef7360f3ece66de130cf50c9f01b7facabbc
tarena06 <tarena@pianyelaiye.local>
2015-07-01 10:23:59 +0800

    Initial Commit

---
+++ 0701-github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  0701-github
+//
+//  Created by tarena06 on 15/7/1.
+//  Copyright (c) 2015年 PPF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
